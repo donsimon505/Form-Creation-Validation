8f5cfbbda49ead91ecdc28058e90018892b1deb6
Stephen Doe <donsimon505@gmail.com>
2025-06-19 02:20:11 +0000

    Setup and Initial Code Structure

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+});
