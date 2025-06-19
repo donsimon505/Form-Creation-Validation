321c3c9209583bb4a6b884fe092a92fea4b67d6a
Stephen Doe <donsimon505@gmail.com>
2025-06-19 03:35:01 +0000

    Final

--- script.js
+++ script.js
@@ -11,47 +11,35 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = document.getElementById('email').value.trim();
         const password = document.getElementById('password').value.trim();
 
-        let isValid = true;
+        var isValid = true;
         const messages = [];
 
         if(username.length < 3){
             isValid = false;
-            username.textContent = messages.push('Username less than 3 characters');
-        }
-        else{
-            username.textContent = '';
+            messages.push('Username less than 3 characters');
         }
 
         if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)){
             isValid = false;
-            email.textContent = messages.push('Email not meeting requirements');
-        }
-        else{
-            email.textContent = '';
+            messages.push('Email not meeting requirements');
         }
 
-        if(password.length >= 8){
+        if(password.length <= 8){
             isValid = false;
-            password.textContent = messages.push('Password less than 8 characters');
-        }
-        else{
-            password.textContent = '';
+            messages.push('Password less than 8 characters');
         }
 
         // Displaying Feedback
         feedbackDiv.style.display = 'block';
 
-        if(isValid = true){
-            feedbackDiv.textContent('Registration successful!');
+        if(isValid){
+            feedbackDiv.textContent = 'Registration successful';
             feedbackDiv.style.color = '#28a745';
         }
         else{
-            feedbackDiv.innerHTML(messages.join("<br>"));
+            feedbackDiv.innerHTML = messages.join('<br>');
             feedbackDiv.style.color = '#dc3545';
         }
-
-        // If all validations pass, submit the form
-        form.submit();
     });
 
 });
