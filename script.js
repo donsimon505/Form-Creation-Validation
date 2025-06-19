313af3be0380600185b808e31ba265a6a2ae1b07
Stephen Doe <donsimon505@gmail.com>
2025-06-19 03:01:45 +0000

    Update Code

--- script.js
+++ script.js
@@ -3,4 +3,51 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        //Form Fields
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        let isValid = true;
+        const messages = ['Username is less than 3 characters', 'Email does not meet requirement', 'Password is less than 8 character'];
+
+        if(username.length < 3){
+            isValid = false;
+            username.textContent = messages[0];
+        }
+        else{
+            username.textContent = '';
+        }
+
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)){
+            isValid = false;
+            email.textContent = messages[1];
+        }
+        else{
+            email.textContent = '';
+        }
+
+        if(password.length >= 8){
+            isValid = false;
+            password.textContent = messages[2];
+        }
+        else{
+            password.textContent = '';
+        }
+
+        // Displaying Feedback
+        feedbackDiv.style.display('block');
+
+        if(isValid = true){
+            feedbackDiv.textContent('Registration successful!');
+            feedbackDiv.style.color('#28a745');
+        }
+        else{
+            feedbackDiv.innerHTML(`${username.textContent}<br>${email.textContent}<br>${password.textContent}`);
+        }
+    });
+
 });
